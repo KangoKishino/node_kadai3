527c3ce9f14494cd1846bb04c45699adea164949
your git username <your git email>
2021-01-02 10:54:47 +0900

    node_kadai3

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -1,19 +1,13 @@
 'use strict';
 
-var request = require('request');
-
-var options = {
-    url: 'https://opentdb.com/api.php?amount=10',
-    method: 'GET',
-    json: true
-}
+const Quizs = require('../models/Quizs');
 
 exports.getIndexPage = (req, res) => {
     res.render('index');
 };
 
 exports.getquizAPI = (req, res) => {
-    request(options, (error, response, body) => {
-        res.json(body);
+    Quizs.init().then((result) => {
+        res.send(result);
     });
 }
